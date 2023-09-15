6f95c9bd72c0ac5a3609556f487ddcea66de34ab
shafi <shafi@LAPTOP-6KCSTHT1>
2023-09-15 14:33:29 +0530

    first commit

---
+++ src/com/svit/daytwo/WhileDemo.java
@@ -0,0 +1,13 @@
+package com.svit.daytwo;
+
+public class WhileDemo {
+	public static void main(String[] args) 
+	{
+		int i=99;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
