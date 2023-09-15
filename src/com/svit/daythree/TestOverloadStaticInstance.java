ef09241c6459de3f715e42aab72b3f76a74ab099
shafi <shafi@LAPTOP-6KCSTHT1>
2023-09-15 15:24:13 +0530

    day3

---
+++ src/com/svit/daythree/TestOverloadStaticInstance.java
@@ -0,0 +1,20 @@
+package com.svit.daythree;
+
+public class TestOverloadStaticInstance {
+	public static void foo() {
+		System.out.println("Test.foo() called ");
+	}
+
+	
+	  public void foo(int a) { // Compiler Error: cannot redefine foo()
+	  System.out.println("Test.foo(int) called "); }
+	 
+	
+	public static void main(String args[]) {
+		TestOverloadStaticInstance.foo();
+		
+		TestOverloadStaticInstance t1 = new TestOverloadStaticInstance();
+		t1.foo(1);
+	}
+
+}
